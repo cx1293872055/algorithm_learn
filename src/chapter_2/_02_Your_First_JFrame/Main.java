10082db1ad061042998ee9ff4dbdf5f9f57dc812
cx1293872055 <1293872055@qq.com>
2019-08-16 13:04:56 +0800

    陈鑫2019年8月16日 13:04:48

---
+++ src/chapter_2/_02_Your_First_JFrame/Main.java
@@ -0,0 +1,14 @@
+package chapter_2._02_Your_First_JFrame;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            // AlgoFrame frame = new AlgoFrame("Welcome", 500, 500);
+            AlgoFrame frame = new AlgoFrame("Welcome");
+        });
+    }
+}
