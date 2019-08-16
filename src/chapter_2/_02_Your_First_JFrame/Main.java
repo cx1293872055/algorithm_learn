08262d8a148d79329d36302621ed116eea632c8a
cx1293872055 <1293872055@qq.com>
2019-08-16 15:25:52 +0800

    陈鑫2019年8月16日 15:25:29 将代码分模块

--- src/chapter_2/_02_Your_First_JFrame/Main.java
+++ src/chapter_2/_02_Your_First_JFrame/Main.java
@@ -1,7 +1,5 @@
 package chapter_2._02_Your_First_JFrame;
 
-import java.awt.*;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,30 +8,7 @@ public class Main {
         int sceneHeight = 800;
 
         int n = 10;
-        Circle[] circles = new Circle[n];
-        int R = 50;
-        for (int i = 0; i < n; i++) {
-            int x = (int)(Math.random()*(sceneWidth - 2 * R)) + R;
-            int y = (int)(Math.random()*(sceneHeight - 2 * R)) + R;
-            int vx = (int) (Math.random() * (11)) - 5;
-            int vy = (int) (Math.random() * (11)) - 5;
-            circles[i] = new Circle(x, y, R, vx, vy);
-        }
 
-        EventQueue.invokeLater(() -> {
-            AlgoFrame frame = new AlgoFrame("Welcome",sceneWidth,sceneHeight);
-            new Thread(() -> {
-                while (true) {
-                    frame.render(circles);
-
-                    AlgoVisHelper.pause(20);
-
-                    for (Circle circle : circles) {
-                        circle.move(0, 0, sceneWidth, sceneHeight);
-                    }
-                }
-            }).start();
-
-        });
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, n);
     }
 }
