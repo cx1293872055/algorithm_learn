4dd28f1f75c5b2949ca86798999634a58a85a9a8
cx1293872055 <1293872055@qq.com>
2019-08-19 18:58:30 +0800

    2019年8月19日 18:58:24

---
+++ src/chapter_2/_05_Maze_Solver/_06_Non_Recursive_DFS_Maze_Solver/Position.java
@@ -0,0 +1,14 @@
+package chapter_2._05_Maze_Solver._06_Non_Recursive_DFS_Maze_Solver;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
