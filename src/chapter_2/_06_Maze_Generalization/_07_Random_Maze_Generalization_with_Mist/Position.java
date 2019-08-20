25c7050d303de9b76cbb897f152f0af168f5abaf
cx1293872055 <1293872055@qq.com>
2019-08-20 13:03:27 +0800

    2019年8月20日 13:03:22

---
+++ src/chapter_2/_06_Maze_Generalization/_07_Random_Maze_Generalization_with_Mist/Position.java
@@ -0,0 +1,14 @@
+package chapter_2._06_Maze_Generalization._07_Random_Maze_Generalization_with_Mist;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
