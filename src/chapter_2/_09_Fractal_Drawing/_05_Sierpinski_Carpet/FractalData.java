df91d87b69efd47ec0bb0c34b34662d4d4a8f19e
cx1293872055 <1293872055@qq.com>
2019-08-22 11:18:08 +0800

    2019年8月22日 11:18:02

---
+++ src/chapter_2/_09_Fractal_Drawing/_05_Sierpinski_Carpet/FractalData.java
@@ -0,0 +1,11 @@
+package chapter_2._09_Fractal_Drawing._05_Sierpinski_Carpet;
+
+public class FractalData {
+
+    public int depth;
+
+    public FractalData(int depth){
+        this.depth = depth;
+    }
+
+}
