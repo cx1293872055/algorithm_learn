8bb75ba9d413ef44a51d10fe685643af43c7c969
cx1293872055 <1293872055@qq.com>
2019-08-22 11:57:49 +0800

    2019年8月22日 11:57:44

---
+++ src/chapter_2/_09_Fractal_Drawing/_08_Tree_Fractal/FractalData.java
@@ -0,0 +1,12 @@
+package chapter_2._09_Fractal_Drawing._08_Tree_Fractal;
+
+public class FractalData {
+
+    public int depth;
+    public double splitAngle;
+
+    public FractalData(int depth, double splitAngle){
+        this.depth = depth;
+        this.splitAngle = splitAngle;
+    }
+}
