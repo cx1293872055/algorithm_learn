61ef800b154e1f33827a32d41cc1a475bb5f9475
cx1293872055 <1293872055@qq.com>
2019-08-22 11:09:57 +0800

    2019年8月22日 11:09:51

---
+++ src/chapter_2/_09_Fractal_Drawing/_03_Fractal_Basics/FractalData.java
@@ -0,0 +1,14 @@
+package chapter_2._09_Fractal_Drawing._03_Fractal_Basics;
+
+public class FractalData {
+
+    private int depth;
+
+    public FractalData(int depth){
+        this.depth = depth;
+    }
+
+    public int getDepth(){
+        return depth;
+    }
+}
---
+++ src/chapter_2/_09_Fractal_Drawing/_04_Fractal_with_Interaction/FractalData.java
@@ -0,0 +1,11 @@
+package chapter_2._09_Fractal_Drawing._04_Fractal_with_Interaction;
+
+public class FractalData {
+
+    public int depth;
+
+    public FractalData(int depth){
+        this.depth = depth;
+    }
+
+}
