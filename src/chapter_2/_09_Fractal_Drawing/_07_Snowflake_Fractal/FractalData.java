dd25f2596c68e0a3ff54adf339fa7d19ec2017cc
cx1293872055 <1293872055@qq.com>
2019-08-22 11:50:41 +0800

    2019年8月22日 11:50:35

---
+++ src/chapter_2/_09_Fractal_Drawing/_07_Snowflake_Fractal/FractalData.java
@@ -0,0 +1,11 @@
+package chapter_2._09_Fractal_Drawing._07_Snowflake_Fractal;
+
+public class FractalData {
+
+    public int depth;
+
+    public FractalData(int depth){
+        this.depth = depth;
+    }
+
+}
